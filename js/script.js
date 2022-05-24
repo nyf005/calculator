5b317cbecc5300744cd3d2b1b7602fbc7aaf29e6
Fabrice N'DRI <nyf005@gmail.com>
2022-05-24 22:40:56 +0000

    Add keyboard support for digits, dot, basic operators, equals, backspace and clear buttons

--- js/script.js
+++ js/script.js
@@ -19,6 +19,35 @@ const delBtn = document.getElementById("del");
 
 const percentageBtn = document.getElementById("percentage");
 
+// Adding keyboard support
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+  // Normal digits, Keypad digits and dot
+  if ((e.key >= 0 && e.key <= 9) || e.key == ".") {
+    inputDigit(e.key);
+  }
+
+  // Arithmetic operators (+, -, *, /)
+  if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/") {
+    inputOperator(e.key);
+  }
+
+  // Equals or Enter
+  if (e.key == "=" || e.key == "Enter") {
+    inputEquals();
+  }
+
+  // Backspace
+  if (e.key == "Backspace") {
+    deleteDigit();
+  }
+
+  // Clear
+  if (e.key == "Clear") {
+    resetCalculator();
+  }
+});
+
 percentageBtn.addEventListener("click", (e) => {
   if (currentNum != null || total != null) {
     let previousTotal = total != null ? total : currentNum;
@@ -28,10 +57,16 @@ percentageBtn.addEventListener("click", (e) => {
   }
 });
 
-numBtns.forEach((numBtn) => numBtn.addEventListener("click", inputDigit));
+numBtns.forEach((numBtn) =>
+  numBtn.addEventListener("click", (event) =>
+    inputDigit(event.target.textContent)
+  )
+);
 
 operationBtns.forEach((operationBtn) => {
-  operationBtn.addEventListener("click", inputOperator);
+  operationBtn.addEventListener("click", (event) =>
+    inputOperator(event.target.textContent)
+  );
 });
 
 equalBtn.addEventListener("click", inputEquals);
@@ -40,163 +75,22 @@ clearBtn.addEventListener("click", () => resetCalculator());
 
 delBtn.addEventListener("click", deleteDigit);
 
-function deleteDigit() {
-  if (displayResult.textContent != 0 && !isEqualsActive && !isOperatorActive) {
-    displayResult.textContent = displayResult.textContent.slice(0, -1);
-    currentNum = Number(displayResult.textContent);
-  } else {
-    resetCalculator();
-  }
+// Functions
+
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function resetCalculator() {
-  total = null;
-  currentNum = null;
-  previousNum = null;
-  operator = null;
-  isOperatorActive = false;
-  displayOperation.textContent = "";
-  displayResult.textContent = "0";
-  dotBtn.classList.remove("disabled");
+function substract(num1, num2) {
+  return num1 - num2;
 }
 
-function round(value, decimals) {
-  return parseFloat(Math.round(value + "e" + decimals) + "e-" + decimals);
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function updateUIAfterCalculation(
-  operator,
-  operand1,
-  operand2,
-  calculationResult
-) {
-  displayResultDiv.classList.remove("focus");
-
-  if (typeof calculationResult == "number") {
-    if (calculationResult.toString().includes("e+")) {
-      displayOperation.textContent = operand1.toString().includes("e+")
-        ? `${round(operand1.toString().split("e+")[0], 8)}E${
-            operand1.toString().split("e+")[1]
-          } ${operator.textContent} ${operand2} =`
-        : `${round(operand1, 8)} ${operator.textContent} ${operand2} =`;
-
-      displayResult.textContent = `${round(
-        calculationResult.toString().split("e+")[0],
-        8
-      )}E${calculationResult.toString().split("e+")[1]}`;
-    } else {
-      displayOperation.textContent = `${round(operand1, 8)} ${
-        operator.textContent
-      } ${operand2} =`;
-      displayResult.textContent = round(calculationResult, 8);
-    }
-  } else {
-    displayOperation.textContent = `${round(operand1, 8)} ${
-      operator.textContent
-    } ${operand2} =`;
-    displayResult.textContent = calculationResult;
-    currentNum = null;
-    previousNum = null;
-  }
-}
-
-function inputEquals() {
-  if (previousNum != null && currentNum != null) {
-    total = operate(
-      operator.getAttribute("data-value"),
-      previousNum,
-      currentNum
-    );
-
-    updateUIAfterCalculation(operator, previousNum, currentNum, total);
-    // Set to false so if user type a digit right after equals everything is cleared (in the )
-    isOperatorActive = false;
-    isEqualsActive = true;
-    dotBtn.classList.remove("disabled");
-  }
-}
-
-function inputOperator(event) {
-  if (currentNum != null) {
-    // Set isOperatorActive to true so we could clear the screen when entering new number after an operator is pressed
-    isOperatorActive = true;
-
-    // set isEqualsActive to false so we can still use the total of an operation after pressing equal sign
-    isEqualsActive = false;
-
-    dotBtn.classList.remove("disabled");
-    displayResultDiv.classList.add("focus");
-    displayOperation.textContent = "";
-
-    if (previousNum != null) {
-      total = operate(
-        operator.getAttribute("data-value"),
-        previousNum,
-        currentNum
-      );
-      updateUIAfterCalculation(operator, previousNum, currentNum, total);
-    }
-    // Save the total of the last operation or the current number in the previousNum variable
-    previousNum = total != null ? total : currentNum;
-    currentNum = null;
-
-    operator = event.target;
-  } else {
-    if (!operator && event.target.getAttribute("data-value") == "-") {
-      // Add minus sign if the first number is negative
-      displayResult.textContent = "-";
-    } else if (
-      operator &&
-      (operator.getAttribute("data-value") == "*" ||
-        operator.getAttribute("data-value") == "/" ||
-        operator.getAttribute("data-value") == "EXP" ||
-        operator.getAttribute("data-value") == "exponent") &&
-      event.target.getAttribute("data-value") == "-"
-    ) {
-      // Add minus sign if the 2nd number is negative
-      displayResult.textContent = "-";
-      // isOperatorActive = false;
-    } else {
-      // Multiple clicks on operator
-      operator = event.target;
-    }
-  }
-}
-
-function inputDigit(event) {
-  //   Reset calculator  if no operator has been pressed after the equals sign
-  if (isEqualsActive) {
-    resetCalculator();
-  }
-
-  // set isEqualsActive to false so when pressing the next digit the display doesn't get cleared
-  isEqualsActive = false;
-
-  populateDisplay(event);
-  displayResultDiv.classList.add("focus");
-  if (displayResult.textContent.includes(".")) {
-    dotBtn.classList.add("disabled");
-  }
-}
-
-function populateDisplay(numBtn) {
-  let keyValue = numBtn.target.textContent;
-
-  //   To prevent removing the 0 if user type on the dot
-  if (
-    (displayResult.textContent == "0" && keyValue != ".") ||
-    (isOperatorActive && displayResult.textContent != "-")
-  ) {
-    // Put 0 before dot if it is pressed without 0
-    displayResult.textContent = isOperatorActive && keyValue == "." ? 0 : "";
-  }
-
-  // Set to false so if we want to enter a number with more digits the screen does not clear each time we click on a number
-  isOperatorActive = false;
-
-  displayResult.textContent += keyValue;
-
-  currentNum = Number(displayResult.textContent);
+function divide(num1, num2) {
+  return num2 == 0 ? "Cannot divide by 0" : num1 / num2;
 }
 
 function operate(...args) {
@@ -234,18 +128,155 @@ function operate(...args) {
   return result;
 }
 
-function add(num1, num2) {
-  return num1 + num2;
+function populateDisplay(digit) {
+  // let keyValue = numBtn.target.textContent;
+
+  //   To prevent removing the 0 if user type on the dot
+  if (
+    (displayResult.textContent == "0" && digit != ".") ||
+    (isOperatorActive && displayResult.textContent != "-")
+  ) {
+    // Put 0 before dot if it is pressed without 0
+    displayResult.textContent = isOperatorActive && digit == "." ? 0 : "";
+  }
+
+  // Set to false so if we want to enter a number with more digits the screen does not clear each time we click on a number
+  isOperatorActive = false;
+
+  displayResult.textContent += digit;
+
+  currentNum = Number(displayResult.textContent);
 }
 
-function substract(num1, num2) {
-  return num1 - num2;
+function inputDigit(digit) {
+  //   Reset calculator  if no operator has been pressed after the equals sign
+  if (isEqualsActive) {
+    resetCalculator();
+  }
+
+  // set isEqualsActive to false so when pressing the next digit the display doesn't get cleared
+  isEqualsActive = false;
+
+  populateDisplay(digit);
+  displayResultDiv.classList.add("focus");
+  if (displayResult.textContent.includes(".")) {
+    dotBtn.classList.add("disabled");
+  }
 }
 
-function multiply(num1, num2) {
-  return num1 * num2;
+function inputOperator(oper) {
+  if (currentNum != null) {
+    // Set isOperatorActive to true so we could clear the screen when entering new number after an operator is pressed
+    isOperatorActive = true;
+
+    // set isEqualsActive to false so we can still use the total of an operation after pressing equal sign
+    isEqualsActive = false;
+
+    dotBtn.classList.remove("disabled");
+    displayResultDiv.classList.add("focus");
+    displayOperation.textContent = "";
+
+    if (previousNum != null) {
+      total = operate(operator, previousNum, currentNum);
+      updateUIAfterCalculation(operator, previousNum, currentNum, total);
+    }
+    // Save the total of the last operation or the current number in the previousNum variable
+    previousNum = total != null ? total : currentNum;
+    currentNum = null;
+
+    operator = oper;
+  } else {
+    if (!operator && oper == "-") {
+      // Add minus sign if the first number is negative
+      displayResult.textContent = "-";
+    } else if (
+      operator &&
+      (operator == "*" ||
+        operator == "/" ||
+        operator.getAttribute("data-value") == "EXP" ||
+        operator.getAttribute("data-value") == "exponent") &&
+      oper == "-"
+    ) {
+      // Add minus sign if the 2nd number is negative
+      displayResult.textContent = "-";
+      // isOperatorActive = false;
+    } else {
+      // Multiple clicks on operator
+      operator = oper;
+    }
+  }
 }
 
-function divide(num1, num2) {
-  return num2 == 0 ? "Cannot divide by 0" : num1 / num2;
+function inputEquals() {
+  if (previousNum != null && currentNum != null) {
+    total = operate(operator, previousNum, currentNum);
+
+    updateUIAfterCalculation(operator, previousNum, currentNum, total);
+    // Set to false so if user type a digit right after equals everything is cleared (in the )
+    isOperatorActive = false;
+    isEqualsActive = true;
+    dotBtn.classList.remove("disabled");
+  }
+}
+
+function updateUIAfterCalculation(
+  operator,
+  operand1,
+  operand2,
+  calculationResult
+) {
+  displayResultDiv.classList.remove("focus");
+
+  if (typeof calculationResult == "number") {
+    if (calculationResult.toString().includes("e+")) {
+      displayOperation.textContent = operand1.toString().includes("e+")
+        ? `${round(operand1.toString().split("e+")[0], 8)}E${
+            operand1.toString().split("e+")[1]
+          } ${operator} ${operand2} =`
+        : `${round(operand1, 8)} ${operator} ${operand2} =`;
+
+      displayResult.textContent = `${round(
+        calculationResult.toString().split("e+")[0],
+        8
+      )}E${calculationResult.toString().split("e+")[1]}`;
+    } else {
+      displayOperation.textContent = `${round(
+        operand1,
+        8
+      )} ${operator} ${operand2} =`;
+      displayResult.textContent = round(calculationResult, 8);
+    }
+  } else {
+    displayOperation.textContent = `${round(
+      operand1,
+      8
+    )} ${operator} ${operand2} =`;
+    displayResult.textContent = calculationResult;
+    currentNum = null;
+    previousNum = null;
+  }
+}
+
+function round(value, decimals) {
+  return parseFloat(Math.round(value + "e" + decimals) + "e-" + decimals);
+}
+
+function resetCalculator() {
+  total = null;
+  currentNum = null;
+  previousNum = null;
+  operator = null;
+  isOperatorActive = false;
+  displayOperation.textContent = "";
+  displayResult.textContent = "0";
+  dotBtn.classList.remove("disabled");
+}
+
+function deleteDigit() {
+  if (displayResult.textContent != 0 && !isEqualsActive && !isOperatorActive) {
+    displayResult.textContent = displayResult.textContent.slice(0, -1);
+    currentNum = Number(displayResult.textContent);
+  } else {
+    resetCalculator();
+  }
 }
